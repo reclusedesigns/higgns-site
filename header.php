9ed388813e7e8f3c855f8bb3accc0851d0881153
Andrew Chin <andrew.chin@squareweave.com.au>
2013-02-21 19:03:33 +1100

    first basic template

---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>Recluse</title>
+	<meta name="author" content="Recluse">
+	
+	<!-- vendor files -->
+	<link rel="stylesheet" href="design/vendor/bootstrap/css/bootstrap.css" type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="design/vendor/bootstrap/css/bootstrap-responsive.css" type="text/css" media="screen" charset="utf-8">
+	<link rel="stylesheet" href="design/vendor/fontello/css/fontello.css" type="text/css" media="screen" charset="utf-8">
+
+	
+	<!-- muay styles -->
+	<link rel="stylesheet" href="design/css/layout.css" type="text/css" media="screen" charset="utf-8">
+	
+	<!-- the javascriptage -->
+	<script src="design/vendor/jquery.js" type="text/javascript" charset="utf-8"></script>
+	<script src="design/vendor/bootstrap/js/bootstrap.js" type="text/javascript" charset="utf-8"></script>
+	<script src="design/scripts/toys.js" type="text/javascript" charset="utf-8"></script>
+	
+	<!-- typekit! -->
+	
+	
+</head>
+<body>
+
+
+
+	<!-- Navbar
+	================================================== -->
+
+	<div class="navbar navbar-static-top">
+	  <div class="navbar-inner">
+	  	<div class="container">
+		    <a class="brand" href="#"><!-- <img src="design/images/header/logo.png" alt="logo"> -->Brand</a>
+		    <ul class="nav pull-right">
+		      <li><a href="#"><i class="icon icon-basket-2"></i> About</a></li>
+		      <li><a href="#"><i class="icon icon-mail-1"></i> Contact</a></li>
+		    </ul>
+	  	</div>
+	  </div>
+	</div>
+
+	
