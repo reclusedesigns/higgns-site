1d76f06080de33ab9f26d1ebfdf2708026a3fab8
Andrew Chin <andrew.chin@squareweave.com.au>
2014-12-17 15:43:21 +1100

    changed the image, added parallax and fade in

--- design/scripts/toys.js
+++ design/scripts/toys.js
@@ -6,12 +6,32 @@ jQuery(document).ready(function($) {
 	});
 
 
-
+	// sticky header
 	$(function() {
 		$(document).ready( function() {
 			$('.sticky').stickUp();
 		});
 	});
 
+	/* parallax */
+	$(window).stellar();
+
+
+	/* Window scroll */
+	$(window).scroll( function(){
+
+		/* Check the location of each desired element */
+		$('.hideme').each( function(i){
+
+			var bottom_of_object = $(this).position().top + $(this).outerHeight();
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+			/* If the object is completely visible in the window, fade it */
+			if( bottom_of_window > bottom_of_object ){
+
+				$(this).animate({'opacity':'1'},400);
+			}
+		});
+	});
 
 });
